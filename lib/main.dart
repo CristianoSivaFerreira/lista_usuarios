e4580b3d7b289125ea5dd23c4bdd7c8bcfae126c
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-01-30 13:22:41 -0300

    refatorando o codigo
    Criando mum multProvider para observar as mudas no projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:provider/provider.dart';
 
 import 'views/user_list.dart';
 
@@ -9,13 +11,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => Users(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: UserList(),
       ),
-      home: UserList(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
